7902c2cd8cec5b1110d0a68012a92319d6e16086
Moses-Morris <mosesmorrisdev@gmail.com>
2023-03-29 18:30:04 +0300

    Use of textContent in js.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 window.onload = () => {
     console.log('This is your first program executing!');
 }
+
+let param = document.querySelector('div');
+param.addEventListener('click', pop);
+
+function pop() {
+    let name = prompt('Enter a new name');
+    param.textContent = "Hello, " + name;
+}
