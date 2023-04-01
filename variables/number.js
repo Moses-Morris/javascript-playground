0ee142d8196eab090cd42b3d1c0a7e3bf80db37c
Moses-Morris <mosesmorrisdev@gmail.com>
2023-04-01 17:52:02 +0300

    Test paractices.

---
+++ variables/number.js
@@ -0,0 +1,25 @@
+//let number;
+
+//console.log(Number.Max_Safe_Integer.toLocaleString());
+
+//console.log(Number.Max_Safe_Integer.toLocaleString);
+console.log(typeof Symbol());
+
+let string = "Surprised Integer";
+console.log(string.slice(4,8));
+console.log(1 + "1");
+//console.log(valueof(12 === "12"));
+
+let orders = 6;
+orders += 8;
+console.log(orders);
+
+orders %=3;
+console.log(orders);
+
+
+const male = false;
+male ? console.log("male"): console.log("female");
+
+const loggedIn = false;
+loggedIn ? console.log("logged in"): console.log("logged out");
